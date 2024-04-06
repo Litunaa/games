9515f1b8e046fcf35366986502952b26912e92b0
litunaa <nubtem1337@gmail.com>
2024-04-06 02:17:10 +0200

    dwadfaw

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let totalCost = 0;
 let selectedGames = 0;
 
+
+const categories = {
+    "Call of Duty®: Black Ops": []
+}
+
 function purchase(price){
     totalCost += price;
     selectedGames++;
@@ -11,4 +16,10 @@ function updateCostDisplay(){
     const costDisplay = document.getElementById("cost");
     const formattedCost = totalCost.toFixed(2);
     costDisplay.textContent = "Total cost: " + "€" + formattedCost  + " " + "(" + selectedGames + " " + "games selected" + ")";
+}
+
+
+
+function search(){
+
 }
