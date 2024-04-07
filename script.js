d0a34675f2d732de8eb6b1e6c51edc3493f90aed
litunaa <nubtem1337@gmail.com>
2024-04-07 11:24:46 +0200

    daw

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ let selectedGames = 0;
 
 
 const categories = {
-    "Call of Duty®: Black Ops": []
+    "Call of Duty®: Black Ops": ["First Person Shooter", "Zombies", "Multiplayer", "Campaign"], 
+    "Call of Duty®: Black Ops II": ["First Person Shooter", "Zombies", "Multiplayer", "Campaign"],
+    "Call of Duty®: Black Ops III": ["First Person Shooter", "Zombies", "Multiplayer", "Campaign"],
+    "Apex Legends": ["Battle Royale", "Multiplayer", "First Person Shooter"],
+    "Counter-Strike 2": ["First Person Shooter", "Multiplayer"],
+    "Coutner-Strike Source": ["First Person Shooter", "Multiplayer"],
+    "Garry's Mod": ["First Person Shooter", "Multiplayer", "Third Person Shooter"],
+    "Dungeon Defenders II": ["Third Person Shooter", "Tower Defense", "Multiplayer"],
+    "Call of Duty: World at War": ["First Person Shooter", "Zombies", "Multiplayer", "Campaign"]
 }
 
 function purchase(price){
@@ -20,6 +28,48 @@ function updateCostDisplay(){
 
 
 
-function search(){
+function filter(){
 
+    const checkboxes = document.querySelectorAll('.fps input[type="checkbox"], .tps input[type="checkbox"], .BR input[type="checkbox"], .td input[type="checkbox"], .zombies input[type="checkbox"], .multi input[type="checkbox"], .campaign input[type="checkbox"]');
+
+
+    const selectedCheckboxes = [];
+
+    checkboxes.forEach(function(checkbox){
+        if (checkbox.checked){
+            const label = checkbox.parentElement.querySelector('label').textContent;
+            selectedCheckboxes.push(label);
+        }
+    });
+
+    const games = document.querySelectorAll('.game');
+
+
+    games.forEach(function(game){
+        const title = game.querySelector('.title').textContent;
+        let hideGame = false;
+
+        selectedCheckboxes.forEach(function(selectedCategory){
+            if (!categories[title].includes(selectedCategory)){
+                hideGame = true;     
+            }
+        });
+
+        if (hideGame){
+            game.style.pointerEvents = 'none';
+            game.style.opacity = '0';
+        }
+        else{
+            game.style.pointerEvents = 'auto';
+            game.style.opacity = '1'; 
+        }
+
+        
+    });
+    
+}
+
+
+function search(){
+    
 }
